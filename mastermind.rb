ed367fb3501e122a87e0896900b19912e60e7b0f
Michael Wong <michael.brian.wong@gmail.com>
2015-10-16 11:00:08 -0600

    game working except telling you positions when incorrect

--- mastermind.rb
+++ mastermind.rb
@@ -1,5 +1,10 @@
 class Mastermind
-  attr_reader :guesses, :answer, :start_time, :end_time
+  attr_reader :guesses,
+              :answer,
+              :start_time,
+              :end_time,
+              :past_guesses,
+              :cheated
 
   def initialize
     start_game
@@ -10,6 +15,8 @@ class Mastermind
     puts 'Welcome to MASTERMIND'
     puts 'Would you like to (p)lay, read the (i)nstructions, or (q)uit?'
     user_input = gets.chomp
+    @cheated = false
+    @past_guesses = []
     @start_time = Time.now
     case
     when user_input == 'p'
@@ -30,7 +37,6 @@ class Mastermind
   def play_game
     system "clear"
     answer
-    puts answer.join
     puts "I have generated a beginner sequence with four elements made up of: "
     puts "(r)ed, (g)reen, (b)lue, and (y)ellow.\n\n"
     puts "Use (q)uit at any time to end the game."
@@ -40,30 +46,64 @@ class Mastermind
   end
 
   def total_time
-    total_seconds = @end_time.strftime("%k%M%S").to_i - @start_time.strftime("%k%M%S").to_i
+    seconds = @end_time.strftime("%S").to_i - @start_time.strftime("%S").to_i
+    minutes = @end_time.strftime("%M").to_i - @start_time.strftime("%M").to_i
+    total_seconds = minutes * 60 + seconds
+    total_minutes = total_seconds / 60
+    final_seconds = total_seconds % 60
+    total = "#{total_minutes.to_s} minutes and " + "#{final_seconds.to_s} seconds"
   end
 
   def guesses
     system "clear"
     if @guess == @answer.join
       @end_time = Time.now
-      puts "You guess the right answer in #{total_time} seconds!"
+      if @cheated == false
+        puts "You guessed the right answer in #{total_time}!"
+      else
+        puts "You guessed the right answer in #{total_time}! Even if you had to cheat :P"
+      end
     elsif @guess == 'c' || @guess == 'cheat'
-      puts "rgby"
+      @cheated = true
+      puts "#{@answer.join}"
+      puts "Please Guess"
+      @guess = gets.chomp.downcase
+      guesses
     elsif @guess == 'q' || @guess == 'quit'
       quit
     elsif @guess.length < 4
-      puts "Guess too short"
+      puts "Guess too short. Please try again."
+      @guess = gets.chomp.downcase
+      guesses
     elsif @guess.length > 4
-      puts "Guess too long"
+      puts "Guess too long. Please try again."
+      @guess = gets.chomp.downcase
+      guesses
     else
-      puts "Incorrect guess or edge case."
+      @past_guesses << @guess
+      @past_guesses.each do |guess|
+        puts "#{guess}"
+      end
+      puts "Incorrect guess. Please try again"
+      @guess = gets.chomp.downcase
+      guesses
     end
   end
 
   def display_instructions
+    system "clear"
     puts 'Try to guess the order of the 4 elements by putting in a guess like:'
     puts 'rgby or RGBY. Guesses are not case sensitive. Good luck!'
+    puts 'M(ain) menu or Q(uit).'
+    choice = gets.chomp.downcase
+    if choice == 'm' || choice == 'main'
+      play_game
+    elsif choice == 'q' || choice == 'quit'
+      quit
+    else
+      puts "Invalid character. Quitting."
+      quit
+    end
   end
 
   def quit
